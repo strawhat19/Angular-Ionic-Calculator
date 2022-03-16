d41aa3a40e6a0c0b7133496b3392b6fc9031bd6c
Rakib Ahmed <rakib987@gmail.com>
2022-03-16 01:09:57 -0400

    calc form

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static("../angular-ionic-calculator"));
 
-app.get("/*", (req, res) => res.sendFile("index.html", { root: "/src/" }));
+app.get("/*", (req, res) => res.sendFile("index.html", { root: "./src/" }));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

9e637bf5d5e37d92e4d7e69ce306760f65423be8
Rakib Ahmed <rakib987@gmail.com>
2022-03-15 23:57:27 -0400

    dist root

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist"));
+app.use(express.static("../"));
 
-app.get("/*", (req, res) => res.sendFile("index.html", { root: "dist/" }));
+app.get("/*", (req, res) => res.sendFile("index.html", { root: "/" }));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

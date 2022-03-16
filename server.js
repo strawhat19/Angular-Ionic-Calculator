f222ea88756d27f4efdc1e09e30197d30d8f6d10
Rakib Ahmed <rakib987@gmail.com>
2022-03-15 23:53:40 -0400

    dist

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/angular-ionic-calculator"));
+app.use(express.static("./dist"));
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/angular-ionic-calculator/" })
-);
+app.get("/*", (req, res) => res.sendFile("index.html", { root: "dist/" }));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

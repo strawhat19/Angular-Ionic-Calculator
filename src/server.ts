24eb8ca8891f736ab69591e75672cabaf6c37ae4
Rakib Ahmed <rakib987@gmail.com>
2022-03-19 05:59:19 -0400

    pre add calendar

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
-//Install express server
-const express = require("express");
-const path = require("path");
-
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static("../angular-ionic-calculator"));
-
-app.get("/*", (req, res) => res.sendFile("server.ts", { root: "./src/" }));
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8100);
-
-console.log(`app`, app);
+// //Install express server
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+// const PORT = process.env.PORT || 8100;
+// // Serve only the static files form the dist directory
+// app.use(express.static("www"));
+// // Start the app by listening on the default Heroku port
+// app.listen(PORT, () => {
+//     console.log(`Listening on Port: ${PORT}`, app);
+// });
